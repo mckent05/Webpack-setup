de7bbab3ff4ef0a54fd8d53f22e07ed79a923afd
MCKENT05 <atemitope95@gmail.com>
2021-10-25 19:11:16 +0100

    Made some changes

--- src/index.js
+++ src/index.js
@@ -3,57 +3,81 @@ import "./style.css";
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { faEllipsisV } from "@fortawesome/free-solid-svg-icons/faEllipsisV";
 import { faTrash } from "@fortawesome/free-solid-svg-icons/faTrash";
+import { faSyncAlt } from "@fortawesome/free-solid-svg-icons/faSyncAlt";
+import { faSignInAlt } from "@fortawesome/free-solid-svg-icons/faSignInAlt";
 library.add(faEllipsisV);
 library.add(faTrash);
+library.add(faSyncAlt);
+library.add(faSignInAlt);
 dom.watch();
 
 const toDoList = [
   {
-    description: "i want to smoke",
+    description: "i want to be great",
     index: 4,
-    completed: true,
+    completed: false,
   },
   {
     description: "i want to laugh",
     index: 3,
-    completed: true,
+    completed: false,
   },
   {
-    description: "i want to shit",
-    index: 1,
-    completed: true,
-  },
-  {
-    description: "i want to fight",
+    description: "i want to have fun",
     index: 2,
-    completed: true,
+    completed: false,
+  },
+  {
+    description: "i want to run",
+    index: 1,
+    completed: false,
   },
   {
     description: "i want to box",
     index: 0,
-    completed: true,
+    completed: false,
   },
 ];
 
 // Lodash, currently included via a script, is required for this line to work
 const element = document.createElement("div");
-document.body.appendChild(element);
 element.classList.add("container");
+document.body.appendChild(element);
+
 const container = document.createElement("ul");
 container.classList.add("list-cont");
 element.appendChild(container);
+
 let clear = document.createElement("button");
 clear.classList.add("clr-todo");
 clear.innerHTML = "clear all completed";
 element.appendChild(clear);
+
+const inputDiv = document.createElement("div");
+inputDiv.classList.add("inputdiv");
+element.appendChild(inputDiv);
+
+const titleDiv = document.createElement("div");
+titleDiv.classList.add("title-cont");
+element.appendChild(titleDiv);
+
 const title = document.createElement("h1");
 title.classList.add("header-title");
 title.textContent = "todays to do";
-element.appendChild(title);
+titleDiv.appendChild(title);
+
+const sync = document.createElement("i");
+sync.classList.add("fa", "fa-sync-alt", "sync");
+titleDiv.appendChild(sync);
+
 const enterInput = document.createElement("input");
 enterInput.classList.add("add-todo");
-enterInput.placeholder = "Add to your list";
-element.appendChild(enterInput);
+enterInput.placeholder = "Add to your list ...";
+inputDiv.appendChild(enterInput);
+
+const enterIcon = document.createElement("i");
+enterIcon.classList.add("fa", "fa-sign-in-alt", "enter");
+inputDiv.appendChild(enterIcon);
 
 window.addEventListener("DOMContentLoaded", () => {
   displayToDo();
@@ -74,29 +98,29 @@ let createToDo = (index) => {
   const list = document.createElement("li");
   list.setAttribute("draggable", "true");
   list.classList.add("to-do");
-  const toDoCont = document.createElement("div");
-  toDoCont.classList.add("todo-cont");
+  container.appendChild(list);
+
   const checkBox = document.createElement("input");
-  const btnDiv = document.createElement("div");
-  const input = document.createElement("input");
-  const to_do = document.createElement("p");
+  checkBox.classList.add("check");
+  checkBox.checked = toDoList[index].completed;
   checkBox.type = "checkbox";
   list.appendChild(checkBox);
-  list.appendChild(toDoCont);
+
+  const btnDiv = document.createElement("div");
   btnDiv.classList.add("btn-cont");
+  list.appendChild(btnDiv);
+
+  const input = document.createElement("input");
+  input.classList.add("task");
+  input.readOnly = true;
+  input.value = toDoList[index].description;
+  list.appendChild(input);
+
   const icon = document.createElement("i");
-  const icon2 = document.createElement("i");
-  btnDiv.appendChild(icon);
-  btnDiv.appendChild(icon2);
   icon.classList.add("fas", "fa-trash", "trash");
+  btnDiv.appendChild(icon);
+
+  const icon2 = document.createElement("i");
   icon2.classList.add("fas", "fa-ellipsis-v", "menu");
-  list.appendChild(btnDiv);
-  to_do.innerHTML = toDoList[index].description;
-  toDoCont.appendChild(input);
-  toDoCont.appendChild(to_do);
-  list.appendChild(btnDiv);
-  container.appendChild(list);
-  checkBox.addEventListener("click", () => {
-    console.log(checkBox.checked);
-  });
+  btnDiv.appendChild(icon2);
 };
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'To-Do List',
+      template: './src/index.html',
     }),
   ],
   output: {
