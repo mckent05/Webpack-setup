cd46a2612617810f1602db20a2577acb3728e972
MCKENT05 <atemitope95@gmail.com>
2021-10-26 10:03:59 +0100

    fixed linter issues

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import "./style.css";
-import { library, dom } from "@fortawesome/fontawesome-svg-core";
-import { faEllipsisV } from "@fortawesome/free-solid-svg-icons/faEllipsisV";
-import { faTrash } from "@fortawesome/free-solid-svg-icons/faTrash";
-import { faSyncAlt } from "@fortawesome/free-solid-svg-icons/faSyncAlt";
-import { faSignInAlt } from "@fortawesome/free-solid-svg-icons/faSignInAlt";
+import './style.css';
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons/faEllipsisV';
+import { faTrash } from '@fortawesome/free-solid-svg-icons/faTrash';
+import { faSyncAlt } from '@fortawesome/free-solid-svg-icons/faSyncAlt';
+import { faSignInAlt } from '@fortawesome/free-solid-svg-icons/faSignInAlt';
 
 library.add(faEllipsisV);
 library.add(faTrash);
@@ -13,27 +13,27 @@ dom.watch();
 
 const toDoList = [
   {
-    description: "i want to be great",
+    description: 'i want to be great',
     index: 4,
     completed: false,
   },
   {
-    description: "i want to laugh",
+    description: 'i want to laugh',
     index: 3,
     completed: false,
   },
   {
-    description: "i want to have fun",
+    description: 'i want to have fun',
     index: 2,
     completed: false,
   },
   {
-    description: "i want to run",
+    description: 'i want to run',
     index: 1,
     completed: false,
   },
   {
-    description: "i want to box",
+    description: 'i want to box',
     index: 0,
     completed: false,
   },
@@ -41,73 +41,73 @@ const toDoList = [
 
 const main = document.querySelector('.main');
 
-const element = document.createElement("div");
-element.classList.add("container");
+const element = document.createElement('div');
+element.classList.add('container');
 main.appendChild(element);
 
-const container = document.createElement("ul");
-container.classList.add("list-cont");
+const container = document.createElement('ul');
+container.classList.add('list-cont');
 element.appendChild(container);
 
-const clear = document.createElement("button");
-clear.classList.add("clr-todo");
-clear.innerHTML = "clear all completed";
+const clear = document.createElement('button');
+clear.classList.add('clr-todo');
+clear.innerHTML = 'clear all completed';
 element.appendChild(clear);
 
-const inputDiv = document.createElement("div");
-inputDiv.classList.add("inputdiv");
+const inputDiv = document.createElement('div');
+inputDiv.classList.add('inputdiv');
 element.appendChild(inputDiv);
 
-const titleDiv = document.createElement("div");
-titleDiv.classList.add("title-cont");
+const titleDiv = document.createElement('div');
+titleDiv.classList.add('title-cont');
 element.appendChild(titleDiv);
 
-const title = document.createElement("h1");
-title.classList.add("header-title");
-title.textContent = "todays to do";
+const title = document.createElement('h1');
+title.classList.add('header-title');
+title.textContent = 'todays to do';
 titleDiv.appendChild(title);
 
-const sync = document.createElement("i");
-sync.classList.add("fa", "fa-sync-alt", "sync");
+const sync = document.createElement('i');
+sync.classList.add('fa', 'fa-sync-alt', 'sync');
 titleDiv.appendChild(sync);
 
-const enterInput = document.createElement("input");
-enterInput.classList.add("add-todo");
-enterInput.placeholder = "Add to your list ...";
+const enterInput = document.createElement('input');
+enterInput.classList.add('add-todo');
+enterInput.placeholder = 'Add to your list ...';
 inputDiv.appendChild(enterInput);
 
-const enterIcon = document.createElement("i");
-enterIcon.classList.add("fa", "fa-sign-in-alt", "enter");
+const enterIcon = document.createElement('i');
+enterIcon.classList.add('fa', 'fa-sign-in-alt', 'enter');
 inputDiv.appendChild(enterIcon);
 
 const createToDo = (index) => {
-  const list = document.createElement("li");
-  list.setAttribute("draggable", "true");
-  list.classList.add("to-do");
+  const list = document.createElement('li');
+  list.setAttribute('draggable', 'true');
+  list.classList.add('to-do');
   container.appendChild(list);
 
-  const checkBox = document.createElement("input");
-  checkBox.classList.add("check");
+  const checkBox = document.createElement('input');
+  checkBox.classList.add('check');
   checkBox.checked = toDoList[index].completed;
-  checkBox.type = "checkbox";
+  checkBox.type = 'checkbox';
   list.appendChild(checkBox);
 
-  const btnDiv = document.createElement("div");
-  btnDiv.classList.add("btn-cont");
+  const btnDiv = document.createElement('div');
+  btnDiv.classList.add('btn-cont');
   list.appendChild(btnDiv);
 
-  const input = document.createElement("input");
-  input.classList.add("task");
+  const input = document.createElement('input');
+  input.classList.add('task');
   input.readOnly = true;
   input.value = toDoList[index].description;
   list.appendChild(input);
 
-  const icon = document.createElement("i");
-  icon.classList.add("fas", "fa-trash", "trash");
+  const icon = document.createElement('i');
+  icon.classList.add('fas', 'fa-trash', 'trash');
   btnDiv.appendChild(icon);
 
-  const icon2 = document.createElement("i");
-  icon2.classList.add("fas", "fa-ellipsis-v", "menu");
+  const icon2 = document.createElement('i');
+  icon2.classList.add('fas', 'fa-ellipsis-v', 'menu');
   btnDiv.appendChild(icon2);
 };
 
@@ -122,6 +122,6 @@ const displayToDo = () => {
   }
 };
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
   displayToDo();
 });
