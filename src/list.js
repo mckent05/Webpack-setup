8a59a7200fff6ca55f2a9223ef1593368ff428bd
MCKENT05 <atemitope95@gmail.com>
2021-10-26 23:57:18 +0100

    fixed linter errors

--- src/list.js
+++ src/list.js
@@ -1,5 +1,14 @@
 const alert = document.querySelector('.alert');
 
+const displayAlert = (message, action) => {
+  alert.innerHTML = message;
+  alert.classList.add(`alert-${action}`);
+  setTimeout(() => {
+    alert.innerHTML = '';
+    alert.classList.remove(`alert-${action}`);
+  }, 3000);
+};
+
 const taskCompleted = (list, index, e) => {
   list[index].completed = e.currentTarget.checked;
   const inner = e.currentTarget.nextSibling.nextSibling;
@@ -19,13 +28,6 @@ const addToLocalStorage = (list) => {
   localStorage.setItem('completedtask', JSON.stringify(completed));
 };
 
-const displayAlert = (message, action) => {
-  alert.innerHTML = message;
-  alert.classList.add(`alert-${action}`);
-  setTimeout(() => {
-    alert.innerHTML = '';
-    alert.classList.remove(`alert-${action}`);
-  }, 3000);
-};
+
 
 export { taskCompleted, addToLocalStorage as localstorage, displayAlert };
